b9a84f5e60c04baa9e7f83fff7f3ebe5d2cd1f2f
Alejandro Orozco <aorozco31096@gmail.com>
2026-03-25 19:10:05 -0400

    added meshing functions

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-import waveguide_fem
+import meshing
+import fem_solver
+import postprocessing
 
+GENERATE_MESH=True
 
 def main():
-    pass
+    mesh_size = 5e-4
+    # Rectangular waveguide dimensions (a and b comparable to WR75 but with nicer metric dimensions)
+    width = 20e-3  # a [m]
+    height = width / 2  # b = a/2
 
+    if GENERATE_MESH:
+        meshing.generate_rectangular_mesh(width, height, mesh_size)
+
+    # Circular waveguide
+    radius = 10e-3
+
+    if GENERATE_MESH:
+        meshing.generate_circular_mesh(radius, mesh_size)
+
+    # Single ridge waveguide
+    ridge_width = 0.4 * width
+    ridge_depth = 0.4 * height
+
+    if GENERATE_MESH:
+        meshing.generate_single_ridge_mesh(width, height, ridge_width, ridge_depth, mesh_size)
+
+    # double ridge waveguide
+    ridge_width = 0.4 * width
+    ridge_depth = 0.35 * height
+
+    if GENERATE_MESH:
+        meshing.generate_double_ridge_mesh(width, height, ridge_width, ridge_depth, mesh_size)
 
 if __name__ == '__main__':
     main()
---
+++ meshing.py
@@ -0,0 +1,238 @@
+import gmsh
+
+DEBUG=True
+
+def generate_rectangular_mesh(a, b, mesh_size):
+    # Initialize Gmsh
+    gmsh.initialize()
+    # Print messages to console
+    gmsh.option.setNumber("General.Terminal", 1)
+    # Create model
+    gmsh.model.add("rectangular_waveguide")
+
+    # Create points for waveguide corners
+    gmsh.model.geo.addPoint(0, 0, 0, mesh_size, 1)
+    gmsh.model.geo.addPoint(a, 0, 0, mesh_size, 2)
+    gmsh.model.geo.addPoint(a, b, 0, mesh_size, 3)
+    gmsh.model.geo.addPoint(0, b, 0, mesh_size, 4)
+
+    # Connect points with lines
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 1, 4)
+
+    # Define curve loop for surface
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+
+    # Create plane surface
+    gmsh.model.geo.addPlaneSurface([1], 1)
+
+    # Synchronize entities
+    gmsh.model.geo.synchronize()
+
+    # Define physical groups for PEC walls and surface
+    gmsh.model.addPhysicalGroup(1, [1, 2, 3, 4], 1)
+    gmsh.model.setPhysicalName(1, 1, "PEC")
+
+    gmsh.model.addPhysicalGroup(2, [1], 2)
+    gmsh.model.setPhysicalName(2, 2, "surface")
+
+    # Generate mesh
+    gmsh.model.mesh.generate(2)
+
+    # Display surfaces and nodes
+    gmsh.option.setNumber('Mesh.SurfaceFaces', 1)
+    gmsh.option.setNumber('Mesh.Points', 1)
+
+    # Save mesh to file
+    gmsh.write('rectangular_waveguide.msh')
+
+    if DEBUG:
+        # Visualize mesh
+        gmsh.fltk.run()
+
+    # Finalize Gmsh
+    gmsh.finalize()
+
+
+def generate_circular_mesh(r, mesh_size):
+    # Initialize Gmsh
+    gmsh.initialize()
+    # Print messages to console
+    gmsh.option.setNumber("General.Terminal", 1)
+    # Create model
+    gmsh.model.add('circular_waveguide')
+    # Create points for circular waveguide
+    gmsh.model.geo.addPoint(r, r, 0, mesh_size, 1)  # Center
+    gmsh.model.geo.addPoint(2*r, r, 0, mesh_size, 2)
+    gmsh.model.geo.addPoint(0, r, 0, mesh_size, 3)
+
+
+    gmsh.model.geo.addCircleArc(2, 1, 3, 1)  # Top half
+    gmsh.model.geo.addCircleArc(3, 1, 2, 2)  # Bottom half
+
+    gmsh.model.geo.addCurveLoop([1, 2], 1)
+
+    # Create plane surface
+    gmsh.model.geo.addPlaneSurface([1], 1)
+
+    # Synchronize entities
+    gmsh.model.geo.synchronize()
+
+    # Define physical groups for PEC walls and surface
+    gmsh.model.addPhysicalGroup(1, [1, 2], 1)
+    gmsh.model.setPhysicalName(1, 1, "PEC")
+
+    gmsh.model.addPhysicalGroup(2, [1], 2)
+    gmsh.model.setPhysicalName(2, 2, "surface")
+
+
+
+    # %% Ahora sí se procede a crear la malla:
+
+    gmsh.model.mesh.generate(2)
+
+    # Display surfaces and nodes
+    gmsh.option.setNumber('Mesh.SurfaceFaces', 1)
+    gmsh.option.setNumber('Mesh.Points', 1)
+
+    # Save mesh to file
+    gmsh.write('circular_waveguide.msh')
+
+    if DEBUG:
+        # Visualize mesh
+        gmsh.fltk.run()
+
+    # Finalize Gmsh
+    gmsh.finalize()
+
+def generate_single_ridge_mesh(a, b, w, d, mesh_size):
+    # Initialize Gmsh
+    gmsh.initialize()
+    # Print messages to console
+    gmsh.option.setNumber("General.Terminal", 1)
+    # Create model
+    gmsh.model.add("single_ridge_waveguide")
+
+    # Create points for waveguide corners
+    gmsh.model.geo.addPoint(0, 0, 0, mesh_size, 1)
+    gmsh.model.geo.addPoint(a, 0, 0, mesh_size, 2)
+    gmsh.model.geo.addPoint(a, b, 0, mesh_size, 3)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, b, 0, mesh_size, 4)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, b - d, 0, mesh_size, 5)
+    gmsh.model.geo.addPoint((a - w) / 2, b - d, 0, mesh_size, 6)
+    gmsh.model.geo.addPoint((a - w) / 2, b, 0, mesh_size, 7)
+    gmsh.model.geo.addPoint(0, b, 0, mesh_size, 8)
+
+    # Connect points with lines
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 5, 4)
+    gmsh.model.geo.addLine(5, 6, 5)
+    gmsh.model.geo.addLine(6, 7, 6)
+    gmsh.model.geo.addLine(7, 8, 7)
+    gmsh.model.geo.addLine(8, 1, 8)
+
+    # Define curve loop for surface
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4, 5, 6, 7, 8], 1)
+
+    # Create plane surface
+    gmsh.model.geo.addPlaneSurface([1], 1)
+
+    # Synchronize entities
+    gmsh.model.geo.synchronize()
+
+    # Define physical groups for PEC walls and surface
+    gmsh.model.addPhysicalGroup(1, [1, 2, 3, 4, 5, 6, 7, 8], 1)
+    gmsh.model.setPhysicalName(1, 1, "PEC")
+
+    gmsh.model.addPhysicalGroup(2, [1], 2)
+    gmsh.model.setPhysicalName(2, 2, "surface")
+
+    # Generate mesh
+    gmsh.model.mesh.generate(2)
+
+    # Display surfaces and nodes
+    gmsh.option.setNumber('Mesh.SurfaceFaces', 1)
+    gmsh.option.setNumber('Mesh.Points', 1)
+
+    # Save mesh to file
+    gmsh.write('single_ridge_waveguide.msh')
+
+    if DEBUG:
+        # Visualize mesh
+        gmsh.fltk.run()
+
+    # Finalize Gmsh
+    gmsh.finalize()
+
+def generate_double_ridge_mesh(a, b, w, d, mesh_size):
+    # Initialize Gmsh
+    gmsh.initialize()
+    # Print messages to console
+    gmsh.option.setNumber("General.Terminal", 1)
+    # Create model
+    gmsh.model.add("double_ridge_waveguide")
+
+    # Create points for waveguide corners
+    gmsh.model.geo.addPoint(0, 0, 0, mesh_size, 1)
+    gmsh.model.geo.addPoint((a - w) / 2, 0, 0, mesh_size, 2)
+    gmsh.model.geo.addPoint((a - w) / 2, d, 0, mesh_size, 3)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, d, 0, mesh_size, 4)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, 0, 0, mesh_size, 5)
+    gmsh.model.geo.addPoint(a, 0, 0, mesh_size, 6)
+    gmsh.model.geo.addPoint(a, b, 0, mesh_size, 7)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, b, 0, mesh_size, 8)
+    gmsh.model.geo.addPoint(a - (a - w) / 2, b - d, 0, mesh_size, 9)
+    gmsh.model.geo.addPoint((a - w) / 2, b - d, 0, mesh_size, 10)
+    gmsh.model.geo.addPoint((a - w) / 2, b, 0, mesh_size, 11)
+    gmsh.model.geo.addPoint(0, b, 0, mesh_size, 12)
+
+    # Connect points with lines
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 5, 4)
+    gmsh.model.geo.addLine(5, 6, 5)
+    gmsh.model.geo.addLine(6, 7, 6)
+    gmsh.model.geo.addLine(7, 8, 7)
+    gmsh.model.geo.addLine(8, 9, 8)
+    gmsh.model.geo.addLine(9, 10, 9)
+    gmsh.model.geo.addLine(10, 11, 10)
+    gmsh.model.geo.addLine(11, 12, 11)
+    gmsh.model.geo.addLine(12, 1, 12)
+
+    # Define curve loop for surface
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 1)
+
+    # Create plane surface
+    gmsh.model.geo.addPlaneSurface([1], 1)
+
+    # Synchronize entities
+    gmsh.model.geo.synchronize()
+
+    # Define physical groups for PEC walls and surface
+    gmsh.model.addPhysicalGroup(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 1)
+    gmsh.model.setPhysicalName(1, 1, "PEC")
+
+    gmsh.model.addPhysicalGroup(2, [1], 2)
+    gmsh.model.setPhysicalName(2, 2, "surface")
+
+    # Generate mesh
+    gmsh.model.mesh.generate(2)
+
+    # Display surfaces and nodes
+    gmsh.option.setNumber('Mesh.SurfaceFaces', 1)
+    gmsh.option.setNumber('Mesh.Points', 1)
+
+    # Save mesh to file
+    gmsh.write('double_ridge_waveguide.msh')
+
+    if DEBUG:
+        # Visualize mesh
+        gmsh.fltk.run()
+
+    # Finalize Gmsh
+    gmsh.finalize()
