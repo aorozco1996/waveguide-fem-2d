4064647c060486a3ba62718d1419db89b1bfcceb
Alejandro Orozco <aorozco31096@gmail.com>
2026-03-28 15:58:28 -0400

    added function to load mesh data

--- fem_solver.py
+++ fem_solver.py
@@ -3,6 +3,22 @@ import numpy as np
 from scipy.sparse.linalg import eigsh
 
 def load_mesh(filename):
+    # Read the .msh file
+    mesh = meshio.read(filename)
+
+    # Get nodes. Only keep x and y coordinates
+    nodes = mesh.points[:, :2]
+
+    # Get elements. Assume only triangular elements
+    elements = mesh.cells_dict["triangle"]
+
+    # Get boundary nodes
+    boundary_lines = mesh.cells_dict["line"]
+    boundary_nodes = np.unique(boundary_lines)
+
+    return nodes, elements, boundary_nodes
+
+def _assemble_local_matrix():
     pass
 
 def assemble_te_matrices():
--- main.py
+++ main.py
@@ -6,8 +6,25 @@ import postprocessing
 GENERATE_MESH=False
 
 def run_rectangular_waveguide(width, height, mesh_size):
+    print('Running Rectangular Waveguide FEM simulation...')
+    print(f'Waveguide dimensions: a = {width} [m], b = {height} [m]')
     if GENERATE_MESH:
+        print('Generating mesh...')
         meshing.generate_rectangular_mesh(width, height, mesh_size)
+
+    nodes, elements, boundary_nodes = fem_solver.load_mesh('rectangular_waveguide.msh')
+
+    print('Mesh Details')
+    print(f'Element size: {mesh_size} [m]')
+    print(f'Total nodes: {len(nodes)}')
+    print(f'Total elements: {len(elements)}')
+    print(f'Total boundary nodes: {len(boundary_nodes)}')
+
+    # print(nodes[:10])
+    # print(elements[:10])
+    # print(boundary_nodes[:10])
+
+
 
 def run_circular_waveguide(radius, mesh_size):
     if GENERATE_MESH:
@@ -29,22 +46,22 @@ def main():
 
     run_rectangular_waveguide(width, height, mesh_size)
 
-    # Circular waveguide
-    radius = 10e-3
-
-    run_circular_waveguide(radius, mesh_size)
-
-    # Single ridge waveguide
-    ridge_width = 0.4 * width
-    ridge_depth = 0.4 * height
-
-    run_single_ridge_waveguide(width, height, ridge_width, ridge_depth, mesh_size)
-
-    # double ridge waveguide
-    ridge_width = 0.4 * width
-    ridge_depth = 0.35 * height
-
-    run_double_ridge_waveguide(width, height, ridge_width, ridge_depth, mesh_size)
+    # # Circular waveguide
+    # radius = 10e-3
+    #
+    # run_circular_waveguide(radius, mesh_size)
+    #
+    # # Single ridge waveguide
+    # ridge_width = 0.4 * width
+    # ridge_depth = 0.4 * height
+    #
+    # run_single_ridge_waveguide(width, height, ridge_width, ridge_depth, mesh_size)
+    #
+    # # double ridge waveguide
+    # ridge_width = 0.4 * width
+    # ridge_depth = 0.35 * height
+    #
+    # run_double_ridge_waveguide(width, height, ridge_width, ridge_depth, mesh_size)
 
 if __name__ == '__main__':
     main()
