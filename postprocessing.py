e8124314e247cfec9d7af6c7fb2cbc32e287ee5f
Alejandro Orozco <aorozco31096@gmail.com>
2026-03-29 19:22:01 -0400

    cleaned up dispersion curves plots

--- postprocessing.py
+++ postprocessing.py
@@ -7,12 +7,12 @@ EPS0 = 8.854187818814e-12  # permittivity of free space [F*m^-1]
 MU0 = 4 * np.pi * 1e-7  # permeability of free space [H/m]
 Z0 = 376.73031341259  # impedance of free space [Ohm]
 
-def plot_dispersion_curves(kc_sq_tm, kc_sq_te, a, b, waveguide_type):
+def plot_dispersion_curves(kc_sq_tm, kc_sq_te, a, b=0, waveguide_type='rectangular'):
     plt.figure()
 
     if waveguide_type == "rectangular":
-        te_labels = ["TE10", "TE01/TE20", "TE11/TM11"]
-        tm_labels = ["TE21/TM21", "TE31/TM31", "TE12/TM12"]
+        te_labels = ["TE$_{10}$", "TE$_{01}$/TE$_{20}$", "TE$_{11}$/TM$_{11}$"]
+        tm_labels = ["TE$_{21}$/TM$_{21}$", "TE$_{31}$/TM$_{31}$", "TE$_{12}$/TM$_{12}$"]
 
         kc_te = np.array([
             np.sqrt((1 * np.pi / a) ** 2 + (0 * np.pi / b) ** 2),
@@ -27,8 +27,8 @@ def plot_dispersion_curves(kc_sq_tm, kc_sq_te, a, b, waveguide_type):
         ])
 
     if waveguide_type == "circular":
-        te_labels = ["TE11", "TE21", "TE31"]
-        tm_labels = ["TM01", "TM11", "TM21"]
+        te_labels = ["TE$_{11}$", "TE$_{21}$", "TE$_{31}$"]
+        tm_labels = ["TM$_{01}$", "TE$_{01}$/TM$_{11}$", "TM$_{21}$"]
 
         kc_te = np.array([
             jnp_zeros(1, 1)[0] / a,
@@ -57,7 +57,7 @@ def plot_dispersion_curves(kc_sq_tm, kc_sq_te, a, b, waveguide_type):
         line, = plt.plot(ka, beta_over_k, label=labels_all[i])
 
         ka_cutoff_num = kc_fem_all[i] * a
-        ka_num = np.linspace(1.02 * ka_cutoff_num, ka_max, 8)
+        ka_num = np.linspace(1.02 * ka_cutoff_num, ka_max, 10)
         beta_over_k_num = np.sqrt(1 - (ka_cutoff_num / ka_num) ** 2)
 
         plt.plot(
@@ -72,14 +72,16 @@ def plot_dispersion_curves(kc_sq_tm, kc_sq_te, a, b, waveguide_type):
 
     plt.xlabel(r"$k a$")
     plt.ylabel(r"$\beta / k$")
-    plt.title(f"Dispersion Curves for a {waveguide_type.capitalize()} Waveguide: Analytical vs FEM")
+    plt.suptitle(f"Dispersion Curves for a {waveguide_type.capitalize()} Waveguide:")
+    plt.title("Analytical vs. FEM Results")
     plt.ylim(0, 1.05)
     plt.xlim(left=0)
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
+    plt.savefig(f"figures/{waveguide_type}_dispersion_curves.png", dpi=300, bbox_inches="tight")
     plt.show()
 
 
-def plot_mode_distribution(field, mode):
-    pass
+def plot_mode_distribution(nodes, elements, field, mode_label, value_type='real', quantity_label=None, levels=40,
+                           cmap='viridis', save_path=None):
