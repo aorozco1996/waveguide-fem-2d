828f7a472bcc3df12268c7406d7a475f971106b7
Alejandro Orozco <aorozco31096@gmail.com>
2026-03-29 19:21:53 -0400

    cleaned up dispersion curves plots

--- main.py
+++ main.py
@@ -34,18 +34,56 @@ def run_rectangular_waveguide(width, height, mesh_size):
     # Omit degenerate modes so that there are 6 unique eigenvalues
     kc_sq_tm_unique = kc_sq_tm[1:]
     Ez_tm_unique = Ez_tm[1:]
-    kc_sq_te_unique = np.concatenate((kc_sq_te[1:2], kc_sq_te[3:]))
 
+    kc_sq_te_unique = np.concatenate((kc_sq_te[1:2], kc_sq_te[3:]))
     Hz_te_unique = np.concatenate((Hz_te[1:2], Hz_te[3:]))
+
     # Plot dispersion curves
-    postprocessing.plot_dispersion_curves(kc_sq_tm_unique, kc_sq_te_unique, width, height, 'rectangular')
+    postprocessing.plot_dispersion_curves(kc_sq_tm_unique, kc_sq_te_unique, width, b=height, waveguide_type='rectangular')
 
     # Plot modal distributions
 
 
 def run_circular_waveguide(radius, mesh_size):
+    print('Running Circular Waveguide FEM simulation...')
+    print(f'Waveguide dimensions: a = {radius} [m]')
     if GENERATE_MESH:
+        print('Generating mesh...')
         meshing.generate_circular_mesh(radius, mesh_size)
+
+    # Load mesh data
+    nodes, elements, boundary_nodes = fem_solver.load_mesh('circular_waveguide.msh')
+
+    print('Mesh Details')
+    print(f'Element size: {mesh_size} [m]')
+    print(f'Total nodes: {len(nodes)}')
+    print(f'Total elements: {len(elements)}')
+    print(f'Total boundary nodes: {len(boundary_nodes)}')
+
+    # Assemble tm and te matrices
+    A_tm, B_tm = fem_solver.assemble_tm_matrices(nodes, elements, boundary_nodes)
+    A_te, B_te = fem_solver.assemble_te_matrices(nodes, elements)
+
+    # Solve eigenvalue problems
+    kc_sq_tm, Ez_tm = fem_solver.solve_eigenvalue_problem(A_tm, B_tm, num_modes=4)
+    kc_sq_te, Hz_te = fem_solver.solve_eigenvalue_problem(A_te, B_te, num_modes=7)
+
+    print(kc_sq_tm)
+    print(kc_sq_te)
+
+    # Omit degenerate modes so that there are 6 unique eigenvalues
+    kc_sq_tm_unique = np.concatenate((kc_sq_tm[:2], kc_sq_tm[3:]))
+    Ez_tm_unique = np.concatenate((Ez_tm[:2], Ez_tm[3:]))
+    kc_sq_te_unique = np.concatenate((kc_sq_te[1:2], kc_sq_te[3:4], kc_sq_te[6:7]))
+    Hz_te_unique = np.concatenate((Hz_te[1:2], Hz_te[3:4], Hz_te[6:7]))
+
+    print(kc_sq_tm_unique)
+    print(kc_sq_te_unique)
+
+    # # Plot dispersion curves
+    postprocessing.plot_dispersion_curves(kc_sq_tm_unique, kc_sq_te_unique, radius, waveguide_type='circular')
+
+    # Plot modal distributions
 
 def run_single_ridge_waveguide(width, height, ridge_width, ridge_depth, mesh_size):
     if GENERATE_MESH:
